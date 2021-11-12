d79084b536910a717ab63efc61629f44daaf6933
J. Alejandro Coronado Cuesta <tkalejandro@Js-MacBook-Pro.fritz.box>
2021-11-12 17:59:57 +0100

    media values updated and div container was transform to an empty react fragment.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Stir It Up</title>
         <meta name="description" content="Mongolian BBQ Social Restaurant in Berlin" />
@@ -31,6 +31,6 @@ export default function Home() {
       </main>
 
       <Footer />
-    </div>
+    </>
   )
 }
