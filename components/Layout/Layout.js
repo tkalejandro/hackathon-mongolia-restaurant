749e10b1b8d88a2b5336ff14f9e1c8e21747768b
loerk <fra.se@posteo.net>
2021-11-14 00:43:56 +0100

    titlePage and Nav

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,12 +2,12 @@ import MainNavigation from "../MainNavigation/MainNavigation"
 import Footer from "../Footer/Footer"
 
 
-
 const Layout = (props) => {
     return (
         <>
         <MainNavigation />
-        <main>{props.children}</main>
+        <main>
+            {props.children}</main>
         <Footer />
         </>
     )
--- components/MainNavigation/MainNavigation.js
+++ components/MainNavigation/MainNavigation.js
@@ -1,25 +1,33 @@
 
 import Link from "next/link"
 import styles from "./MainNavigation.module.css"
+import logoBlack from "../../images/logoBlack.png"
+import Image from "next/image"
 
 const MainNavigation = () => {
     return (
         <header className={styles.header}>
             <div className={styles.navContainer}>
-            <div className={styles.logo}>Stir It Up</div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href='/order-online'>Order</Link>
-                    </li>
-                </ul>
-            </nav>
+                <div className={styles.logoContainer}>
+                <Image
+                    src={logoBlack}
+                    alt="Logo"
+                    className={styles.logoBlack}
+                /></div>
+                <nav className={styles.mainNav}>
+                    <ul className={styles.mainNavList}>
+                        <li className={styles.mainNavLinks}>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li className={styles.mainNavLinks}>
+                            <Link href='/order-online'>Order</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     )
 }
+
 
 export default MainNavigation
